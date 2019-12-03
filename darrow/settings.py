caa4f7379f7200de3ceb2139693c0cfb7e648c61
grrdsoto <grrdnazar@gmail.com>
2019-12-02 23:16:29 -0500

    changed settings

--- darrow/settings.py
+++ darrow/settings.py
@@ -20,13 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't^_ivn++_emq*nva!g+qwurmn-xgyyg9+6c(s89p5m3)y388mk'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['darrow-gstr410.herokuapp.com']
 
 # Application definition
 
