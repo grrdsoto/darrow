ef4e6ea08c8a005e95c683fb809a51c372bcceb9
grrdsoto <grrdnazar@gmail.com>
2019-12-03 00:36:15 -0500

    added whitenoise to wsgi.py

--- darrow/wsgi.py
+++ darrow/wsgi.py
@@ -12,5 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'darrow.settings')
+from whitenoise.django import DjangoWhiteNoise
 
-application = get_wsgi_application()
+application = DjangoWhiteNoise(get_wsgi_application())
